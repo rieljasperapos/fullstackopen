69e08e8559d4d65c74ea4ae737d6dc42f0c90305
Riel <jasper.apos@gmail.com>
2023-12-24 02:17:41 +0800

    exercise: 1.8 unicafe

--- part1/unicafe/unicafe/src/App.tsx
+++ part1/unicafe/unicafe/src/App.tsx
@@ -1,32 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Statistics from './components/Statistics';
 
-interface FeedbackState {
+interface StatisticsData {
   good: number;
   neutral: number;
   bad: number;
+  totalFeedback: number;
+  average: number;
+  percentage: number;
 }
 
-const initialState = { good: 0, neutral: 0, bad: 0};
+const initialStatisticsState: StatisticsData = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  totalFeedback: 0,
+  average: 0,
+  percentage: 0,
+};
 
 function App() {
-  const [feedback, setFeedback] = useState<FeedbackState>(initialState);
-  const [totalFeedback, setTotalFeedback] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [percentage, setPercentage] = useState(0);
+  const [statistics, setStatistics] = useState<StatisticsData>(initialStatisticsState);
 
-  const handleFeedbackClick = (type: keyof FeedbackState) => {
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      [type]: prevFeedback[type] + 1,
-    }));
-    setTotalFeedback(totalFeedback + 1);
+  const handleFeedbackClick = (type: keyof StatisticsData) => {
+    setStatistics((prevStatistics) => {
+      const newStatistics = {
+        ...prevStatistics,
+        [type]: prevStatistics[type] + 1,
+        totalFeedback: prevStatistics.totalFeedback + 1
+      };
+
+      const newAverage = newStatistics.totalFeedback === 0 ? 0 : (newStatistics.good - newStatistics.bad) / newStatistics.totalFeedback;
+      const newPercentage = newStatistics.totalFeedback === 0 ? 0 : (newStatistics.good / newStatistics.totalFeedback) * 100;
+
+      return {
+        ...newStatistics,
+        average: newAverage,
+        percentage: newPercentage,
+      };
+    });
   };
 
-  useEffect(() => {
-    setAverage(totalFeedback === 0 ? 0 : (feedback.good - feedback.bad) / totalFeedback);
-    // setPercentage((feedback.good / totalFeedback) * 100)
-    setPercentage(totalFeedback === 0 ? 0 : (feedback.good / totalFeedback) * 100)
-  }, [feedback, totalFeedback])
+  console.log(statistics);
 
   return (
     <>
@@ -38,13 +53,7 @@ function App() {
           <button onClick={() => handleFeedbackClick('bad')}>bad</button>
         </div>
         <div>
-          <h1>Statistics</h1>
-          <p>good {feedback.good}</p>
-          <p>neutral {feedback.neutral}</p>
-          <p>bad {feedback.bad}</p>
-          <p>all {totalFeedback}</p>
-          <p>average {average}</p>
-          <p>positive {percentage} %</p>
+          <Statistics statistics={statistics} />
         </div>
       </div>
     </>
---
+++ part1/unicafe/unicafe/src/components/Statistics.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface StatisticsProps {
+    statistics: {
+        good: number;
+        neutral: number;
+        bad: number;
+        totalFeedback: number;
+        average: number;
+        percentage: number;
+    };
+}
+const Statistics: React.FC<StatisticsProps> = (props) => {
+    console.log(props.statistics.good);
+    return (
+        <>
+            <div>
+                <h1>Statistics</h1>
+                <p>good {props.statistics.good}</p>
+                <p>neutral {props.statistics.neutral}</p>
+                <p>bad {props.statistics.bad}</p>
+                <p>all {props.statistics.totalFeedback}</p>
+                <p>average {props.statistics.average}</p>
+                <p>positive {props.statistics.percentage} %</p>
+            </div>
+        </>
+    )
+}
+
+export default Statistics;
